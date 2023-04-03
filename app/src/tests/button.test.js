d219b718b99cf6dece69dea4d364578d84283c79
OniDem1 <spaindecraft17@gmail.com>
2023-04-03 08:46:59 +0700

    Начал писать тест кнопок

---
+++ app/src/tests/button.test.js
@@ -0,0 +1,8 @@
+import Button from '../Button';
+ /*
+describe('Тест кнопок', () => { 
+    test('Renders the button component without crashing', () => {
+        render(<Button />);
+    });      
+})
+*/
