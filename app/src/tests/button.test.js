bc1235cd28335357648c0efab62e12db49702575
VuacheslavMironov <vuacheslav.mir@gmail.com>
2023-04-07 10:17:36 +0700

    Сделал тест на кнопки

--- app/src/tests/button.test.js
+++ app/src/tests/button.test.js
@@ -1,18 +1,5 @@
-import { mount } from '@vue/test-utils'
-import  App from './app.vue'
+const loginView = require('../views/SignInView.vue')
 
-  describe('Mounted App', () => {
-    const wrapper = mount(App);
-  
-    test('does a wrapper exist', () => {
-      expect(wrapper.exists()).toBe(true)
-    })
-    it('renders the correct markup', () => {
-      expect(wrapper.html()).toContain('What is the sum of the two numbers?')
-    })
-    it('has a button', () => {
-      expect(wrapper.find('ButtonComponent').exists()).toBe(true)
-    })
-  })
-
-  
+test('My first test', () => {
+  expect(loginView['default']['components']['ButtonComponent']);
+});
