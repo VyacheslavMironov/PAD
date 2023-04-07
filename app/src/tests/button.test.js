222a2871cda58d82426e7cd6d86c7db21dbc1bfc
OniDem1 <spaindecraft17@gmail.com>
2023-04-07 15:22:02 +0700

    тест кнопкок

--- app/src/tests/button.test.js
+++ app/src/tests/button.test.js
@@ -1,8 +1,18 @@
-import Button from '../Button';
- /*
-describe('Тест кнопок', () => { 
-    test('Renders the button component without crashing', () => {
-        render(<Button />);
-    });      
-})
-*/
+import { mount } from '@vue/test-utils'
+import  App from './app.vue'
+
+  describe('Mounted App', () => {
+    const wrapper = mount(App);
+  
+    test('does a wrapper exist', () => {
+      expect(wrapper.exists()).toBe(true)
+    })
+    it('renders the correct markup', () => {
+      expect(wrapper.html()).toContain('What is the sum of the two numbers?')
+    })
+    it('has a button', () => {
+      expect(wrapper.find('ButtonComponent').exists()).toBe(true)
+    })
+  })
+
+  
